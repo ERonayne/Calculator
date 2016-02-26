a507e6da7a3c76ce27d97fac112d1848677dda7b
Evan Ronayne <ronaynee@msoe.edu>
2016-02-26 08:33:04 -0600

    Add exit command and extracted string conversion

--- src/Main.java
+++ src/Main.java
@@ -11,44 +11,61 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         Boolean exit = false;
         while(!exit) {
-            System.out.println("Enter a command: ");
-            // try {
+            System.out.println("Enter a command, \"exit\" to quit: ");
             String input = scan.nextLine();
             String[] tokens = input.split(" ");
+
+            //add, mul, his, div, etc
             String command = tokens[0];
+
+            //convert input into integers
             List<Integer> integers = new ArrayList<Integer>();
-            for (int i = 1; i < tokens.length; i++) {
-                try {
-                    int integer = Integer.parseInt(tokens[1]);
-                    integers.add(integer);
-                } catch (NumberFormatException e) {
-                    System.err.println("Invalid format!");
-                    continue;
-                }
+            if(!convertStringsToInt(tokens, integers)) {
+                continue;
             }
+
             Calculator calc = new Calculator();
             String result = "";
             switch (command) {
                 case "add":
                     result = "" + calc.add(integers);
+                    System.out.println(result);
                     break;
                 case "sub":
                     result = "" + calc.subtract(integers);
+                    System.out.println(result);
                     break;
                 case "mul":
                     result = "" + calc.multiply(integers);
+                    System.out.println(result);
                     break;
                 case "div":
                     result = "" + calc.divide(integers);
+                    System.out.println(result);
                     break;
                 case "his":
                     Stack<String> hist = calc.getHistory();
+                    System.out.println(result);
+                    break;
+                case "exit":
+                    exit = true;
                     break;
                 default:
-                    System.out.println(command);
-                    break;
+                    System.err.println("unknown format!");
             }
-            System.out.println(result);
         }
+    }
+
+    private static boolean convertStringsToInt(String[] tokens, List<Integer> integers) {
+        for (int i = 1; i < tokens.length; i++) {
+            try {
+                int integer = Integer.parseInt(tokens[i]);
+                integers.add(integer);
+            } catch (NumberFormatException e) {
+                System.err.println("Invalid format!");
+                return false;
+            }
+        }
+        return true;
     }
 }
